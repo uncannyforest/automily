07d24065da3796cf34a100b56b71c182fea0ed64
Uncanny Forest <git@uncannyforest.com>
2021-04-15 12:46:04 -0400

    Add private currentuser API for testing
     per https://web.archive.org/web/20181208161754if_/https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const util = require('util')
 const bcrypt = require('bcryptjs')
 const keys = require('../../config/keys')
+const passport = require('passport')
 
 const genSalt = util.promisify(bcrypt.genSalt)
 const hash = util.promisify(bcrypt.hash)
@@ -94,5 +95,20 @@ router.post('/login', async (req, res) => {
     return res.status(400).json({ passwordincorrect: 'Password incorrect' })
   }
 })
+
+// @route GET api/users/currentuser
+// @desc Return current user
+// @access Private
+router.get(
+  '/currentuser',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    })
+  }
+)
 
 module.exports = router

f9306b387f2d98f6902dc27a7c5b9c1c94eef97c
Uncanny Forest <git@uncannyforest.com>
2021-04-19 15:46:27 -0400

    Add get posts API

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,9 +8,9 @@ const validatePostInput = require('../../validation/post')
 // Load User model
 const Post = require('../../models/Post')
 
-// @route POST api/users/create
-// @desc Register user
-// @access Public
+// @route POST api/posts/create
+// @desc Create post
+// @access Private
 router.post(
   '/create',
   passport.authenticate('jwt', { session: false }),
@@ -31,5 +31,26 @@ router.post(
     return res.json(savedPost)
   }
 )
+
+// @route GET api/posts/list
+// @desc Return all posts
+// @access Public
+router.get('/list', async (req, res) => {
+  const posts = await Post.find({})
+
+  return res.json(posts)
+})
+
+// @route GET api/posts/:id
+// @desc Get post
+// @access Public
+router.get('/:id', async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id })
+  if (!post) {
+    return res.status(404).json({ postnotfound: 'Post not found' })
+  }
+
+  return res.json(post)
+})
 
 module.exports = router
